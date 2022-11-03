020663f6e8eecdf547c214bddcccb7bc77b9f2a4
Vova Vykhrystenko <mikserwowa@gmail.com>
2022-11-03 16:16:23 +0200

    Revert "add files"
    This reverts commit 43e280c489904b48e411af0c78cc16690211c9a8.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,16 @@
-import { lazy, Suspense } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-
-const Header = lazy(() => import('../components/Header/Header'));
-const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
-const MoviesPage = lazy(() => import('../pages/MoviesPage/MoviesPage'));
-const MovieDetails = lazy(() => import('../components/MovieDetails/MovieDetails'));
-const Cast = lazy(() => import('../components/Cast/Cast'));
-const Reviews = lazy(() => import('../components/Reviews/Reviews'));
-
 export const App = () => {
   return (
-    <BrowserRouter basename='/goit-react-hw-05-movies/'>
-      <Header />
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='movies' element={<MoviesPage />} />
-          <Route path='movies/:movieId' element={<MovieDetails />}>
-            <Route path='cast' element={<Cast />} />
-            <Route path='reviews' element={<Reviews />} />
-          </Route>
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
   );
 };
